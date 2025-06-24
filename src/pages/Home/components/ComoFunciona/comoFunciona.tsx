3b3d9b053473270304e05c653f4e1f983bd5c54b
Antonio Bezerra <bezerra3a@gmail.com>
2025-06-24 10:35:22 -0300

    feat: Estilizado a parte 'como funciona'

--- src/pages/Home/components/ComoFunciona/comoFunciona.tsx
+++ src/pages/Home/components/ComoFunciona/comoFunciona.tsx
@@ -1,11 +1,11 @@
 import Section from "../Section/Section";
 import comoFuncionaImg from "../../../../assets/Home/comoFunciona.png";
-
+import style from "./comoFunciona.module.scss"
 
 function ComoFunciona() {
 
     return (
-        <section className="ComoFunciona">
+        <section className={style.ComoFunciona}>
             <Section
                 titulo="Como funciona?"
                 texto=" A telemedicina utiliza plataformas digitais seguras, como a CallMed,
@@ -18,7 +18,7 @@ function ComoFunciona() {
           remoto, segunda opinião e apoio a regiões distantes, tornando o
           atendimento mais acessível e eficiente."
             />
-            <img className="ComoFunciona__Imagem" src={comoFuncionaImg} />
+            <img className={style.ComoFunciona__Imagem} src={comoFuncionaImg} />
         </section>
     )
 
