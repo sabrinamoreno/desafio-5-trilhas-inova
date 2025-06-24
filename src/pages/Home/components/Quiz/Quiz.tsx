33ffb1cf385282c15e1107308d6c278e4f3259cf
Antonio Bezerra <bezerra3a@gmail.com>
2025-06-24 11:19:00 -0300

    feat: Estilizado Quiz

--- src/pages/Home/components/Quiz/Quiz.tsx
+++ src/pages/Home/components/Quiz/Quiz.tsx
@@ -1,9 +1,13 @@
+import style from "./Quiz.module.scss"
+
 function Quiz() {
 
     return (
         <section className="Quiz">
-            <h2 className="Quiz__subtitulo">Prove seus conhecimentos em Telemedicina!</h2>
-            <a href="#" className="Quiz__cta" id="botao__quiz">
+            <h2 className={style.Quiz__subtitulo}>
+                Prove seus conhecimentos em Telemedicina!
+            </h2>
+            <a href="#" className={style.Quiz__cta} id="botao__quiz">
                 Responda um Quiz agora!
             </a>
         </section>
