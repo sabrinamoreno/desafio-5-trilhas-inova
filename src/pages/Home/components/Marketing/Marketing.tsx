37f919bbe4b85645e1e94a9046a66fd041da8fd3
Antonio Bezerra <bezerra3a@gmail.com>
2025-06-24 12:46:07 -0300

    feat: Estilizado a parte de Marketing

--- src/pages/Home/components/Marketing/Marketing.tsx
+++ src/pages/Home/components/Marketing/Marketing.tsx
@@ -1,20 +1,21 @@
 import Section from "../Section/Section";
 import ConhecaCallMed from '../../../../assets/Home/conhecaCallMed.png';
+import style from "./Marketing.module.scss"
 
 function Marketing() {
 
     return (
-        <section className="Marketing">
-            <div className="Marketing__Container">
+        <section className={style.Marketing}>
+            <div className={style.Marketing__Container}>
                 <Section
                     titulo="Conheça a CallMed com um clique"
                     texto="Acesse médicos de diversas especialidades com rapidez, segurança e
-            sem sair de casa. Cadastre-se e aproveite todos os benefícios da
-            telemedicina com conforto e agilidade."
+                    sem sair de casa. Cadastre-se e aproveite todos os benefícios da
+                    telemedicina com conforto e agilidade."
                 />
-                <a href="/login" className="Marketing__Container__Cta"> Cadastre-se! </a>
+                <a href="/login" className={style.Marketing__Container__Cta}> Cadastre-se! </a>
             </div>
-            <img className="Marketing__Imagem" src={ConhecaCallMed} />
+            <img className={style.Marketing__Imagem} src={ConhecaCallMed} />
         </section >
     )
 
