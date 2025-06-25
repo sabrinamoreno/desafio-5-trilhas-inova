a904862b289697afec0e4e01647d3a673a142377
Antonio Bezerra <bezerra3a@gmail.com>
2025-06-25 08:19:44 -0300

    feat: Formulario estilizado

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,12 @@
 import Formulario from "./components/Formulario/Formulario";
 import Marca from '../../assets/Login/marca.png';
+import style from "./Login.module.scss"
 
 
 function Login (){
 
     return(
-        <main className="principal">
+        <main className={style.principal}>
             <div className="principal__cabecalho">
                 <img src={Marca} />
                 <h1 className="principal__cabecalho__titulo">CallMed Atendimentos</h1>
