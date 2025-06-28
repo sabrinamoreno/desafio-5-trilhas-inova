f022f334ac9706c0e2faa09e05f53b098eeb6a93
Antonio Bezerra <bezerra3a@gmail.com>
2025-06-28 16:38:16 -0300

    feat: ajustes no estilo de login e cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,21 +1,25 @@
 import Marca from '../../assets/Login/marca.png';
 import FormularioCadastro from './components/Formulario/FormularioCadastro';
 import style from './Cadastro.module.scss';
+import { useEffect } from 'react';
 
 function Cadastro() {
 
+    useEffect(() => {
+        document.body.classList.add(style.cadastro__body);
+        return () => document.body.classList.remove(style.cadastro__body);
+    }, []);
+
     return (
-        <body className={style.cadastro__body}>
-            <div className={style.cadastro}>
-                <div className={style.cadastro__cabecalho}>
-                    <img src={Marca} alt="Logo CallMed" />
-                    <h1 className={style.cadastro__cabecalho__titulo}>
-                        CallMed Atendimentos
-                    </h1>
-                </div>
-                <FormularioCadastro />
+        <section className={style.cadastro}>
+            <div className={style.cadastro__cabecalho}>
+                <img src={Marca} alt="Logo CallMed" />
+                <h1 className={style.cadastro__cabecalho__titulo}>
+                    CallMed Atendimentos
+                </h1>
             </div>
-        </body>
+            <FormularioCadastro />
+        </section>
     )
 
 
