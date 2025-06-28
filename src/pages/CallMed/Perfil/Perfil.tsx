22a1fcd87f50c5e8cdc4be6f0dbfe0c5f9118620
sabrinamoreno <sabrinamorenorodr28741@gmail.com>
2025-06-28 07:26:47 -0300

    feat: Adiciona responsividade mobile-First para Inicio, ComoFunciona,  Marketing , Quiz, e Section

--- src/pages/CallMed/Perfil/Perfil.tsx
+++ src/pages/CallMed/Perfil/Perfil.tsx
@@ -1,7 +1,20 @@
 function Perfil (){
 
     return (
-        <h1>Perfil</h1>
+        <div>
+            <h1>Perfil</h1>
+
+            <p>Informações Pessoais</p>
+
+            <label>Nome Completo</label>
+            <input type="text" placeholder="Digite seu nome completo" />
+
+            <label>E-mail</label>
+            <input type="email" placeholder="Digite seu email" />   
+
+            <label>CPF</label>
+            <input type="number" placeholder="Digite seu CPF" />
+        </div>
     )
 }
 
