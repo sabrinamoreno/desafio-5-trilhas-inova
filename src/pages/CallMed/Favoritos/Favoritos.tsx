fe60f05ebeb8adb26fe4dbc5a633cc6843e712e5
gerson-diniiz <gersonmoises100@gmail.com>
2025-06-29 21:49:44 -0300

    feat: ajustando estilização de Favoritos

--- src/pages/CallMed/Favoritos/Favoritos.tsx
+++ src/pages/CallMed/Favoritos/Favoritos.tsx
@@ -1,50 +1,19 @@
 import Cabecalho from "../components/Cabecalho/Cabecalho";
 import style from "./Favoritos.module.scss";
-import { getMedicosFavoritos } from "./api.simulada";
-import React, { useEffect, useState } from "react";
-import favoritos from "../../../assets/CallMed/star.png";
+import ListaFavoritos from "./components/ListaFavoritos/ListaFavoritos";
 
-type Medico = {
-  id: number;
-  nome: string;
-  especialidade?: string;
-  email?: string;
-  telefone?: string;
-  crm?: string;
-  preco_consulta?: string;
-};
+
 
 export default function Favoritos() {
-  const [medicos, setMedicos] = useState<Medico[]>([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getMedicosFavoritos();
-      setMedicos(data);
-    }
-    fetchData();
-  }, []);
+ 
 
   return (
-    <>
-      <Cabecalho titulo="Médicos Favoritos" />
-      <div className={style.Favoritos}>
-        <h1 className={style.titulo__favoritos}>Médicos Favoritos</h1>
-        <img src={favoritos} alt="estrela do(s) medico(a) favorito(a)" />
+    <div className={style.favoritos}>
+      <Cabecalho 
+      titulo="Médicos Favoritos" />
+      <div className={style.favoritos__corpo}>
+      <ListaFavoritos />
       </div>
-      <div className={style.lista__medicos}>
-        {medicos.map((medico) => (
-          <div className={style.card} key={medico.id}>
-            <div className={style.info}>
-              <h2 className={style.medico__nome}>{medico.nome}</h2>
-              <p className={style.especialidade}>{medico.especialidade}</p>
-            </div>
-            <button className={style.botao} type="submit">
-              Agendar Consulta
-            </button>
-          </div>
-        ))}
-      </div>
-    </>
+    </div>
   );
 }
